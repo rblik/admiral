1d83c5418dc3ffa26d2b9d5176fba690bef1769e
Blik <rblick@bk.ru>
2017-04-06 23:04:09 +0300

    login form

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -7,8 +7,6 @@ import {Credentials} from "../model/credentials";
 
 @Injectable()
 export class AuthService {
-  private loggedEmployee: Subject<Employee> = new BehaviorSubject<Employee>(null);
-  private static TOKEN = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuYW1lMUBnbWFpbC5jb20iLCJjcmVhdGVkIjoxNDkxMzUxNTIzNzM0fQ.LGVyUGLNkarbcC8_x7bWIqmVBq8YFkjxyZUDMKlat2Fqewg2s-uybORXY8aibdbIYzLLzDlDbAB8rBW3Y2mD5Q";
 
   constructor(private http: Http, private localSt: SessionStorageService) {
     this.storeProfile();
@@ -25,27 +23,17 @@ export class AuthService {
   }
 
   public storeProfile(): void {
-    this.http.get("http://localhost:8080/profile", this.getOptionsS()).map(res => res.json()).catch(e=>Observable.throw('Unauthorized'))
+    this.http.get("http://localhost:8080/profile", this.getOptions()).map(res => res.json()).catch(e=>Observable.throw('Unauthorized'))
       .subscribe(employee => {
         // this.loggedEmployee.next(employee);
         this.localSt.store("employee", JSON.stringify(employee));
       });
   }
 
-  public getOptionsS():RequestOptions {
+  public getOptions():RequestOptions {
     let headers = new Headers({'Content-Type': 'application/json'});
     headers.append("Authorization", this.getToken());
     return new RequestOptions({headers: headers});
-  }
-
-  public getOptions(): RequestOptions {
-    let headers = new Headers({'Content-Type': 'application/json'});
-    headers.append("Authorization", AuthService.TOKEN);
-    return new RequestOptions({headers: headers});
-  }
-
-  public getLoggedWorker(): Subject<Employee> {
-    return this.loggedEmployee;
   }
 
   public getToken(): string {
